2be2e81290288a9b1aeae2e90072bb8bb1e95451
Mirmuhsin Sodiqov <mirmukhsin.sodikov@xenoss.io>
2025-09-12 20:36:27 +0500

    Remove unused BidSourceResult subclasses
    Deleted NoFill, TransientFailure, and TrafficControl subclasses from BidSourceResult as they are no longer needed.

--- sdk/src/main/java/io/cloudx/sdk/internal/ads/BidSourceResult.kt
+++ sdk/src/main/java/io/cloudx/sdk/internal/ads/BidSourceResult.kt
@@ -2,10 +2,6 @@ package io.cloudx.sdk.internal.ads
 
 import io.cloudx.sdk.Destroyable
 
-// Bid-level policy outcomes the banner cares about
 internal sealed class BidSourceResult<T : Destroyable> {
     data class Success<T : Destroyable>(val response: BidAdSourceResponse<T>) : BidSourceResult<T>()
-    class NoFill<T : Destroyable> : BidSourceResult<T>()
-    data class TransientFailure<T : Destroyable>(val message: String) : BidSourceResult<T>()
-    data class TrafficControl<T : Destroyable>(val message: String) : BidSourceResult<T>()
 }

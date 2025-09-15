97c985b3af7b330f06a5fbda99919a918b26efda
Steffan <steffan@cloudx.io>
2025-09-15 10:13:55 -0700

    remove banner infinite retries (#30)
    * Removing infinite banner level retries
    
    * Removing infinite banner level retries

--- sdk/src/main/java/io/cloudx/sdk/internal/util/Result.kt
+++ sdk/src/main/java/io/cloudx/sdk/internal/util/Result.kt
@@ -1,10 +1,18 @@
 package io.cloudx.sdk.internal.util
 
 /**
- * @suppress
  * Helper class, purposed mainly as an explicit success/failure function result.
  */
 sealed class Result<R, E> {
     data class Success<R, E>(val value: R) : Result<R, E>()
     data class Failure<R, E>(val value: E) : Result<R, E>()
+
+    public val isSuccess: Boolean get() = this is Success
+    public val isFailure: Boolean get() = this is Failure
+
+    fun successOrNull(): R? =
+        when (this) {
+            is Failure -> null
+            is Success -> this.value
+        }
 }

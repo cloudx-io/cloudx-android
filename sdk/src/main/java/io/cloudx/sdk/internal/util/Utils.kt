63769e554edb873daf1e8a6153f56dcee8b1a13f
Steffan Chartrand <steffan@cloudx.io>
2025-09-16 11:58:51 -0700

    cleanup

--- sdk/src/main/java/io/cloudx/sdk/internal/util/Utils.kt
+++ sdk/src/main/java/io/cloudx/sdk/internal/util/Utils.kt
@@ -13,3 +13,23 @@ internal fun dpToPx(dp: Int, density: Float): Int = (dp * density).toInt()
 internal fun pxToDp(px: Int, density: Float): Int = (px / density).toInt()
 
 fun utcNowEpochMillis() = System.currentTimeMillis()
+
+fun normalizeAndHash(dataToHash: String, algo: String = "md5"): String {
+    val normalized = dataToHash.trim().lowercase()
+    val digest = when (algo) {
+        "md5" -> "MD5"
+        "sha1" -> "SHA-1"
+        "sha256" -> "SHA-256"
+        else -> throw IllegalArgumentException("Unsupported hash algorithm: $algo")
+    }
+    return java.math.BigInteger(
+        1, java.security.MessageDigest.getInstance(digest)
+            .digest(normalized.toByteArray())
+    ).toString(16).padStart(
+        when (digest) {
+            "MD5" -> 32
+            "SHA-1" -> 40
+            else -> 64 // SHA-256
+        }, '0'
+    )
+}
